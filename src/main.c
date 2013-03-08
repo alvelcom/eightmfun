15cb0f6028fc83298ac1fbc6d367790b5c3da131
Paul Khusainov <alvelcom@gmail.com>
2013-03-08 21:40:29 +0400

    More comments

--- src/main.c
+++ src/main.c
@@ -31,13 +31,16 @@ typedef enum qtype
     T_UNSUPPORTED = 128
 } qtype_t;
 
+/*
+ * Parsing states enum
+ */
 typedef enum state
 {
     S_ = 1,
     S_G,
     S_GE,
     S_GET,
-    S_URL, //
+    S_URL, 
     S_URL_,
     S_H,
     S_HT,
@@ -46,15 +49,15 @@ typedef enum state
     S_HTTP_,
     S_HTTP_1,
     S_HTTP_1_,
-    S_HTTP_1_X, //
+    S_HTTP_1_X, 
     S_HTTP_1_Xr,
     S_NEWLINE,
     S_KEY,
-    S_KEY_, //
+    S_KEY_, 
     S_VALUE,
     S_VALUEr, 
-    S_CVALUE, //
-    S_CVALUEr, //
+    S_CVALUE, 
+    S_CVALUEr, 
     S_R,
     S_RN,
 
@@ -69,6 +72,11 @@ typedef struct zstr
     ssize_t pos;
 } zstr_t;
 
+/*
+ * struct mydata
+ *
+ * We pass it to epoll as user-defined payload.
+ */
 typedef struct mydata 
 {
     int     fd;
@@ -76,21 +84,43 @@ typedef struct mydata
     zstr_t  filename;
     zstr_t  buf;
 
+    /*
+     * Really don't know why we should keep double-ended queue...
+     * Maybe someone will implement comet/websockets and 
+     * make one another chat-room-server? :)
+     */
     struct  mydata *next, *prev;
 
+    /*
+     * qtype_t seems to be 8bit length
+     * -- escape padding
+     */
     qtype_t type;
 
 } mydata_t;
 
+/*
+ * Head of DEQ
+ */
 mydata_t *head;
 
+/*
+ * Some finite state machine we use for parsing http.
+ *
+ * State transition table here
+ */
 state_t automata[S_LAST][256];
 
-// forwards
+/*
+ * Forward declarations
+ */
 void init_automata();
 int handle(mydata_t *);
 int answerHTTP(mydata_t *ptr);
 
+/*
+ * Macro for appending one character to the tail of zstr
+ */
 #define APPEND(c, zs)                                       \
     do {                                                    \
         if ((zs).pos + 1 >= (zs).size) {                    \
@@ -134,14 +164,20 @@ main()
     struct epoll_event  ev, events[MAX_EVENTS];
     struct mydata       *data;
 
-    // Init http parser automata
+    /*
+     * Initialize http parser automata
+     */
     init_automata();
 
-    // Create socket
+    /*
+     * Create socket
+     */
     if (0 == (listen_s = socket(AF_INET, SOCK_STREAM, 0)))
         error(1, "Bad socket call");
 
-    // Bind socket
+    /*
+     * Bind socket
+     */
     memset(&addr, 0, sizeof(addr));
     addr.sin_family      = AF_INET;
     addr.sin_port        = htons(9999);
@@ -151,24 +187,32 @@ main()
     if (bind(listen_s, (struct sockaddr *) &addr, socklen))
         error(2, "Bad bind call");
 
-    // And listen
+    /*
+     * And listen
+     */
     if (-1 == listen(listen_s, BACK_LOG))
         error(2, "Bad listen call");
 
-    // Set up epoll
+    /*
+     * Set up epoll
+     */
     if (-1 == (epollfd = epoll_create(10))) /* See NOTES of man epoll_create */
         error(3, "Bad epoll_create call");
 
-    // Attach socket to epoll
-    ev.events   = EPOLLIN;  // We wait for avail. read(2) operations
+    /*
+     * Attach socket to epoll
+     */
+    ev.events   = EPOLLIN;  /* We wait for avail. read(2) operations */
     ev.data.ptr = 0; 
     if (-1 == epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_s, &ev))
         error(4, "Bad epoll_ctl call");
 
     for (;;)
     {
-        // Wait for a new event
-        // The last argument to epoll_wait is timeout (in ms)
+        /*
+         * Wait for a new event
+         * The last argument to epoll_wait is timeout (in ms)
+         */
         if (-1 == (nfsd = epoll_wait(epollfd, events, MAX_EVENTS, -1)))
             error(5, "Bad epoll_wait call");
 
@@ -205,18 +249,21 @@ main()
                  * Now we have one more fd. What will we do with it?
                  * We feed up it to epoll! 
                  */
-                /*
-                 * See man epoll(4) about Level-Triggered and Edge-Triggered
-                 */
-                ev.events = EPOLLIN | EPOLLET;
-
                 data = calloc(1, sizeof(mydata_t));
                 data->fd = conn_s;
                 data->state = S_;
+
+                /*
+                 * Insert to deq
+                 */
                 if (data->prev = head)
                     head->next = data;
                 head = data;
 
+                /*
+                 * See man epoll(4) about Level-Triggered and Edge-Triggered
+                 */
+                ev.events = EPOLLIN | EPOLLET;
                 ev.data.ptr = data;
                 if (-1 == epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_s, &ev))
                     error(7, "Bad epoll_ctl call on conn_s socket");
@@ -244,32 +291,46 @@ handle(mydata_t *ptr)
 
     newstate = ptr->state;
 
+    /*
+     * Reader cycle
+     */
     while (size = read(ptr->fd, buf, RCV_BUF))
     {
         if (-1 == size)
         {
+            /*
+             * I want to catch up the situation when
+             * no available data in socket exists... ughm...
+             *
+             * How to do it right?
+             */
             if (11 == errno)
                 break;
+
             error(0, "Bad read call");
             goto close_conn;
         }
 
         for (pos = buf; pos < buf + size; ++pos)
         {
+            /*
+             * Lookup for a new state
+             */
             oldstate = newstate;
             newstate = automata[oldstate][(unsigned char)*pos];
 
-            if (S_GET == oldstate && S_URL == newstate)
-            {
-                ptr->type = T_GET0;
-                ptr->filename.pos = 0;
-            }
-            
             switch (newstate)
             {
             case S_URL:
                 if (oldstate == newstate)
+                {
                     APPEND(*pos, ptr->filename);
+                }
+                else
+                {
+                    ptr->type = T_GET0;
+                    ptr->filename.pos = 0;
+                }
                 break;
             case S_URL_:
                 APPEND(0, ptr->filename);
@@ -307,17 +368,34 @@ handle(mydata_t *ptr)
             if (S_ERROR == newstate)
                 goto bad_method;
 
-            //printf("%d[%c = %02x] -> %d\n", oldstate, *pos, *pos, newstate);
+            /*
+             * if the final state reached
+             * then generate answer!
+             */
             if (S_RN == newstate)
             {
+                /*
+                 * If answerHTTP return zero then we should
+                 * reset current state and wait for next query.
+                 *
+                 * Otherwise we close connection
+                 */
                 if (answerHTTP(ptr))
                     goto close_conn;
                 else
+                {
+                    ptr->type = T_UNDEF;
+                    ptr->buf.pos = 0;
+                    ptr->filename.pos = 0;
                     newstate = S_;
+                }
             }
         }
     }
 
+    /*
+     * Save the state
+     */
     ptr->state = newstate;
 
     return 0;
@@ -349,6 +427,9 @@ bad_method:
     }
 
 close_conn:
+    /*
+     * Remove from deq
+     */
     if (ptr->next)
         ptr->next->prev = ptr->prev;
     if (ptr->prev)
@@ -357,6 +438,9 @@ close_conn:
     if (head == ptr)
         head = ptr->prev;
 
+    /*
+     * Close the connection and free the memory
+     */
     close(ptr->fd);
     if (ptr->filename.str) free(ptr->filename.str);
     if (ptr->buf.str)      free(ptr->buf.str);
@@ -365,14 +449,17 @@ close_conn:
     return 0;
 }
 
-void fill(state_t state, state_t to_state)
+/**********/
+void
+fill(state_t state, state_t to_state)
 {
     int i;
     for (i = 0; i < 256; i++)
         automata[state][i] = to_state;
 }
 
-void init_automata()
+void
+init_automata()
 {
     int i;
     for (i = 0; i < S_LAST; i++)
@@ -400,12 +487,12 @@ void init_automata()
 
     fill(    S_NEWLINE,           S_KEY);
     automata[S_NEWLINE  ]['\r'] = S_R;
-    automata[S_R        ]['\n'] = S_RN; // the end
+    automata[S_R        ]['\n'] = S_RN; /* the end */
 
     fill(    S_KEY,               S_KEY);
     automata[S_KEY      ][':' ] = S_KEY_;
     automata[S_KEY_     ][' ' ] = S_VALUE;
-    // but if KEY == 'Connection' then newstate = S_CVALUE
+    /* but if KEY == 'Connection' then newstate = S_CVALUE */
     fill(    S_VALUE,             S_VALUE);
     fill(    S_CVALUE,            S_CVALUE);
     automata[S_VALUE    ]['\r'] = S_VALUEr;
@@ -414,7 +501,7 @@ void init_automata()
     automata[S_CVALUEr  ]['\n'] = S_NEWLINE;
 }
 
-/////
+/**********/
 int answerHTTP(mydata_t *ptr)
 {
     char        answer[2048];
@@ -425,73 +512,131 @@ int answerHTTP(mydata_t *ptr)
     struct      stat stat;
     char        closeafter;
 
+    /*
+     * Build answer headers in 
+     *   char answer[2048];
+     * After appending don't forget to move `pos` forward.
+     *
+     * If requested file exists then we should send it.
+     * HINT: try to request /../../../../../../etc/passwd
+     */
+
     closeafter = 0;
     pos = answer;
 
+    /*
+     * Proto version
+     */
     if (ptr->type & T_GET0)
         strcpy(pos, "HTTP/1.0 ");
     else
         strcpy(pos, "HTTP/1.1 ");
-
     while (*pos) pos++;
 
+    /*
+     * Here we decide what to do.
+     * Maybe we should send a file?
+     * Maybe just an index string?
+     */
     filename = ptr->filename.str + 1;
     filed = -1;
-    if (*filename)
+    if (*filename) /* we have filename (i.e. filename == "") */
     {
+        /*
+         * Trying to open file
+         *
+         * At this moment we don't bother about permissions 
+         * and other stuff. Just think that if it un-openable
+         * that it doesnot exists :)
+         */
         if (-1 == (filed = open(filename, O_RDONLY)))
         {
             strcpy(pos, "404 File not found\r\n");
             ans = "File not found\r\n";
         }
+        /*
+         * We also need know about file size
+         */
         else if (-1 == fstat(filed, &stat))
         {
             error(0, "Bad fstat call");
             return 1;
         }
         else
-        {
             strcpy(pos, "200 OK\r\n");
-        }
     }
     else
     {
+        /*
+         * "GET / HTTP" requested
+         * Just print an index string
+         */
         strcpy(pos, "200 OK\r\n");
         ans = "Some page here\r\n";
     }
     while (*pos) pos++;
 
+    /*
+     * Sing the server
+     */
     strcpy(pos, "Server: eightmfun/0.0.0.0.0.1\r\nContent-Type: ");
     while (*pos) pos++;
 
+    /*
+     * Spec. content type ...
+     */
     if (-1 != filed)
         strcpy(pos, "application/octet-stream\r\n");
     else
         strcpy(pos, "text/plain\r\n");
     while (*pos) pos++;
 
+    /*
+     * ... and content length.
+     *
+     * Since this super server doesnot support chunked encoding
+     * we must supply content length.
+     */
     if (-1 != filed)
         sprintf(pos, "Content-Length: %zu\r\n", stat.st_size);
     else
         sprintf(pos, "Content-Length: %zu\r\n", strlen(ans));
     while (*pos) pos++;
 
+    /*
+     * What we will do with connection after sending answer?
+     * Browser should wondering after.
+     */
     if (ptr->type & T_CLOSE || !(ptr->type & T_GET1))
         strcpy(pos, "Connection: close\r\n"), closeafter = 1;
     else
         strcpy(pos, "Connection: keep-alive\r\n");
     while (*pos) pos++;
     
+    /*
+     * Empty line -- end of headers here
+     */
     strcpy(pos, "\r\n");
     while (*pos) pos++;
 
+    /*
+     * We have a little body... Send it with headers
+     */
     if (-1 == filed)
         strcpy(pos, ans);
     while (*pos) pos++;
 
+    /*
+     * Sending headers (and a little body possibly) itself
+     */
     if (-1 == write(ptr->fd, answer, pos - answer))
         error(0, "Bad write");
 
+    /*
+     * Here we sending file over net.
+     *
+     * We using sendfile (works like a pump from one fd to another)
+     */
     if (-1 != filed)
     {
         sendfile(ptr->fd, filed, NULL, stat.st_size);
@@ -499,3 +644,4 @@ int answerHTTP(mydata_t *ptr)
     }
     return closeafter;
 }
+
